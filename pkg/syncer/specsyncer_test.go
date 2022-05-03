2b4efbe878657a720f928daef90a5e93db639ab6
Joaquim Moreno <jmprusi@keepalive.io>
2022-05-03 11:54:49 +0200

    fixup! Support for Advanced Scheduling in Syncer

---
+++ pkg/syncer/specsyncer_test.go
@@ -0,0 +1,422 @@
+package syncer
+
+import (
+	workloadv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/workload/v1alpha1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"testing"
+)
+
+func TestDeepEqualApartFromStatus(t *testing.T) {
+	type args struct {
+		a interface{}
+		b interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "both objects are equals",
+			args: args{
+				a: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+							"labels": map[string]interface{}{
+								"test_label": "test_value",
+							},
+						},
+					},
+				},
+				b: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+							"labels": map[string]interface{}{
+								"test_label": "test_value",
+							},
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "both objects are equals as are being deleted",
+			args: args{
+				a: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":              "test_kind",
+						"apiVersion":        "test_version",
+						"deletionTimestamp": "2010-11-10T23:00:00Z",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+						},
+					},
+				},
+				b: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":              "test_kind",
+						"apiVersion":        "test_version",
+						"deletionTimestamp": "2010-11-10T23:00:00Z",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "both objects are equals even they have different statuses",
+			args: args{
+				a: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":              "test_kind",
+						"apiVersion":        "test_version",
+						"deletionTimestamp": "2010-11-10T23:00:00Z",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+						},
+						"status": map[string]interface{}{},
+					},
+				},
+				b: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":              "test_kind",
+						"apiVersion":        "test_version",
+						"deletionTimestamp": "2010-11-10T23:00:00Z",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+						},
+						"status": map[string]interface{}{
+							"phase": "Failed",
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "both objects are equals even they have a different value in InternalClusterStatusAnnotationPrefix",
+			args: args{
+				a: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+							"labels": map[string]interface{}{
+								"test_label": "test_value",
+							},
+							"annotations": map[string]interface{}{
+								workloadv1alpha1.InternalClusterStatusAnnotationPrefix: "2",
+							},
+						},
+					},
+				},
+				b: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+							"labels": map[string]interface{}{
+								"test_label": "test_value",
+							},
+							"annotations": map[string]interface{}{
+								workloadv1alpha1.InternalClusterStatusAnnotationPrefix: "1",
+							},
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "not equal as b is missing labels",
+			args: args{
+				a: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+							"labels": map[string]interface{}{
+								"test_label": "test_value",
+							},
+						},
+					},
+				},
+				b: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+						},
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "not equal as b has different labels values",
+			args: args{
+				a: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+							"labels": map[string]interface{}{
+								"test_label": "test_value",
+							},
+						},
+					},
+				},
+				b: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+							"labels": map[string]interface{}{
+								"test_label": "another_test_value",
+							},
+						},
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "not equal as b resource is missing annotations",
+			args: args{
+				a: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+							"labels": map[string]interface{}{
+								"test_label": "test_value",
+							},
+							"annotations": map[string]interface{}{
+								"annotation": "this is an annotation",
+							},
+						},
+					},
+				},
+				b: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+							"labels": map[string]interface{}{
+								"test_label": "test_value",
+							},
+						},
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "not equal as b resource has different annotations",
+			args: args{
+				a: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+							"labels": map[string]interface{}{
+								"test_label": "test_value",
+							},
+							"annotations": map[string]interface{}{
+								"annotation": "this is an annotation",
+							},
+						},
+					},
+				},
+				b: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+							"labels": map[string]interface{}{
+								"test_label": "test_value",
+							},
+							"annotations": map[string]interface{}{
+								"annotation":  "this is an annotation",
+								"annotation2": "this is another annotation",
+							},
+						},
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "not equal as b resource has finalizers",
+			args: args{
+				a: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+							"labels": map[string]interface{}{
+								"test_label": "test_value",
+							},
+						},
+					},
+				},
+				b: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+							"labels": map[string]interface{}{
+								"test_label": "test_value",
+							},
+							"finalizers": []interface{}{
+								"finalizer.1",
+								"finalizer.2",
+							},
+						},
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "not equal even objects are equal as A is being deleted",
+			args: args{
+				a: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":              "test_name",
+							"namespace":         "test_namespace",
+							"deletionTimestamp": "2010-11-10T23:00:00Z",
+							"labels": map[string]interface{}{
+								"test_label": "test_value",
+							},
+							"annotations": map[string]interface{}{
+								"test_annotation": "test_value",
+							},
+							"finalizers": []interface{}{
+								"finalizer.1",
+								"finalizer.2",
+							},
+						},
+					},
+				},
+				b: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+							"labels": map[string]interface{}{
+								"test_label": "test_value",
+							},
+							"annotations": map[string]interface{}{
+								"test_annotation": "test_value",
+							},
+							"finalizers": []interface{}{
+								"finalizer.1",
+								"finalizer.2",
+							},
+						},
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "not equal as b has additional fields than a",
+			args: args{
+				a: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+						},
+					},
+				},
+				b: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+						},
+						"other_key": "other_value",
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "not equal as b is not unstructured",
+			args: args{
+				a: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "test_kind",
+						"apiVersion": "test_version",
+						"metadata": map[string]interface{}{
+							"name":      "test_name",
+							"namespace": "test_namespace",
+						},
+					},
+				},
+				b: map[string]interface{}{
+					"test": "test",
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := deepEqualApartFromStatus(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("deepEqualApartFromStatus() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
